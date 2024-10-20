058e01968e8e96e2231e3855ca253c556692b278
Laurie Laberge <laurie.laberge@outlook.com>
2024-10-20 15:22:54 -0400

    getBuyHoldSellSuggestion tests

---
+++ utils/__tests__/stock-option-recommendation.utils.test.ts
@@ -0,0 +1,56 @@
+import { BuyHoldSell } from "../../app/models/stock-option.models"
+import { SocialMediaEnum, SocialMediaMentions } from "../../app/models/social-media.models";
+import { getBuyHoldSellSuggestion } from "../stock-option-recommendation.utils";
+
+describe('getBuyHoldSellSuggestion', () => {
+    const enoughMentions: SocialMediaMentions[] = [
+        {
+            name: SocialMediaEnum.FACEBOOK,
+            nbMentions: 4
+        },
+        {
+            name: SocialMediaEnum.INSTAGRAM,
+            nbMentions: 5
+        },
+        {
+            name: SocialMediaEnum.TIKTOK,
+            nbMentions: 4
+        },
+    ];
+
+    const notEnoughtMentions = [
+        {
+            name: SocialMediaEnum.FACEBOOK,
+            nbMentions: 4
+        },
+        {
+            name: SocialMediaEnum.TWITTER,
+            nbMentions: 5
+        },
+    ];
+
+
+    it.each([
+        [BuyHoldSell.BUY, 200, 200, 200, enoughMentions],
+        [BuyHoldSell.BUY, 200, 200, 150, notEnoughtMentions],
+        [BuyHoldSell.HOLD, 200, 150, 200, notEnoughtMentions],
+        [BuyHoldSell.SELL, 150, 200, 150, notEnoughtMentions],
+    ])('returns recommendation %p with price %p, purchasePrice %p and priceInLastMonth %p', (
+        expectedRecommendation: BuyHoldSell,
+        price: number,
+        purchasePrice: number,
+        priceInLastMonth: number,
+        mentions: SocialMediaMentions[],
+    ) => {
+        const recommendation = getBuyHoldSellSuggestion(
+            mentions,
+            price,
+            purchasePrice,
+            priceInLastMonth
+        )
+
+        expect(recommendation).toEqual(expectedRecommendation)
+    })
+
+
+})
